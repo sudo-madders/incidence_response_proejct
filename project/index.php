8b9062074c9d4c86db7f201f5b49acf7b6030d03
Daave11 <49002232+Daave11@users.noreply.github.com>
2025-04-07 17:37:40 +0200

    Fixed some issues

--- project/index.php
+++ project/index.php
@@ -1,26 +1,25 @@
 <?php
-include('database.php');
+include('library/database.php');
 if (isset($_POST['username']) and isset($_POST['password'])) {
 	$name = $mysqli->real_escape_string($_POST['username']);
 	$pwd = $mysqli->real_escape_string($_POST['password']);
 	$query = <<<END
-
 	SELECT username, password, id FROM users
 	WHERE username = '{$name}'
 	AND password = '{$pwd}'
 	END;
 	$result = $mysqli->query($query);
 	if ($result->num_rows > 0) {
-	$row = $result->fetch_object();
-	$_SESSION["username"] = $row->username;
-	$_SESSION["userId"] = $row->id;
-	header("Location:incident_dashboard.php");
+		$row = $result->fetch_object();
+		$_SESSION["username"] = $row->username;
+		$_SESSION["userId"] = $row->id;
+		header("Location:incident_dashboard.php");
 	} else {
 	echo "Wrong username or password. Try again";
 	}
 }
 $content = <<<END
-<form action="login.php" method="post">
+<form action="index.php" method="POST">
 <input type="text" name="username" placeholder="username">
 <input type="password" name="password" placeholder="password">
 <input type="submit" value="Login">
--- project/library/database.php
+++ project/library/database.php
@@ -1,9 +1,10 @@
 <?php 
-session_name('Website'); 
-session_start(); 
 $host       = "localhost"; 
 $user       = "isacli24";
 $pwd        = "FV0t2Wgb0b";
 $db         = "isacli24";
 $mysqli     = new mysqli($host, $user, $pwd, $db);
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
 ?>
