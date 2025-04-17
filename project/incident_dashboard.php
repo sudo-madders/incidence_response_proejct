f7b122adbbafee130a8db019017b288f7a09f826
isacli24 <isacli24@student.hh.se>
2025-04-17 15:20:48 +0200

    changed php code now works with database

--- project/incident_dashboard.php
+++ project/incident_dashboard.php
@@ -1,4 +1,35 @@
 <?php 
+
+include('library/database.php');
+
+if (isset($_POST['incident_type'], $_POST['severity'], $_POST['description'])) {
+    $incident_type = $mysqli->real_escape_string($_POST['incident_type']);
+    $severity = $mysqli->real_escape_string($_POST['severity']);
+    $description = $mysqli->real_escape_string($_POST['description']);
+
+   
+
+
+
+
+    $query = "INSERT INTO incident (incident_type_ID, severity_ID, description) 
+              VALUES ((SELECT incident_type_ID FROM incident_type WHERE incident_type = '{$incident_type}'),(SELECT severity_ID FROM severity WHERE severity = '{$severity}'), '$description')";
+
+    
+    if ($mysqli->query($query) === TRUE) {
+    header('Location: incident_dashboard.php');
+    exit;
+} else {
+    echo("Could not query database: " . $mysqli->errno . " : " . $mysqli->error);
+}
+
+    
+    header('Location: incident_dashboard.php');
+    exit;
+}
+?>
+
+<?php 
 include("template.php");
 ?>
 				<!-- Main content -->
@@ -16,26 +47,27 @@ include("template.php");
 							</div>
 							<div class="offcanvas-body ">
 								<!-- Här börjar själva panelen -->
-								<form>
+								<form method="post" action="incident_dashboard.php">
 									<div class="row">
 										<div class="col-md-6">
 											<label for="incident_type" class="form-label">Incident Type</label>
-											<select id="incident_type" class="form-select">
+											<select class="form-select" name="incident_type" required>
 												<option selected disabled>Choose incident type</option>
-												<option>Unauthorized access</option>
-												<option>Data breach</option>
-												<option>Malware infection</option>
-												<option>Denial-of-service</option>
-												<option>Insider threat</option>
-												<option>Social engineering attack</option>
-												<option>Physical security breache</option>
-												<option>Compliance violation</option>
+												<option value="Unauthorized access attacks">Unauthorized access attacks</option>
+												<option value="Man-in-the-middle">Man-in-the-middle</option>
+												<option value="Theft">Theft</option>
+												<option value="Denial of service">Denial of service</option>
+												<option value="Insider threats">Insider threats</option>
+												<option value="Ransomware">Ransomware</option>
+												<option value="Privilege escalation">Privilege escalation</option>
+												<option value="Phishing attack">Phishing attack</option>
+												<option value="Password attack">Password attack</option>
 											</select>
 											
 											
 											<br>
-											Affected assets
-											<br>
+											<p>Affected assets</p>
+											
 											
 											<input type="checkbox" name="affected_assets[]" value="inventory" id="affected_inventory">
 											<label for="affected_inventory">Inventory</label>
@@ -59,12 +91,12 @@ include("template.php");
 										
 										<div class="col-md-6">
 											<label for="severity" class="form-label">Severity</label>
-											<select id="severity" class="form-select">
+											<select class="form-select" name="severity" required>
 												<option selected disabled>Choose severity</option>
-												<option>Low</option>
-												<option>Medium</option>
-												<option>High</option>
-												<option>Critical</option>
+												<option value="Low">Low</option>
+												<option value="Medium">Medium</option>
+												<option value="High">High</option>
+												<option value="Critical">Critical</option>
 											</select>
 										</div>
 									</div>
@@ -145,6 +177,9 @@ include("template.php");
 				</div>
 			</div>
 		</div>
-<?php 
+		
+		
+
+<?php
 echo $footer;
 ?>
