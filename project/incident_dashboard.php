9346b29ee3c92285311cf27e0822fe95272641f2
Daave11 <49002232+Daave11@users.noreply.github.com>
2025-04-25 11:55:51 +0200

    Fixade empty space

--- project/incident_dashboard.php
+++ project/incident_dashboard.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <?php
-=======
-<?php 
->>>>>>> 8940896886c657fc74b20f641789380fdf376f9f
 include("template.php");
 include('library/database.php');
 include('filter_incidents.php');
@@ -71,12 +67,6 @@ if (isset($_POST['incident_type'], $_POST['severity'], $_POST['description'])) {
     }
 }
 ?>
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 8940896886c657fc74b20f641789380fdf376f9f
 				<!-- Main content -->
 				<div class="col">
 					<div class="row mb-3 border">
