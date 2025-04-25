78469e81dc32e91324d071c192b94060973197e4
Daave11 <49002232+Daave11@users.noreply.github.com>
2025-04-25 13:39:55 +0200

    Change button size

--- project/template.php
+++ project/template.php
@@ -14,20 +14,26 @@ $currentPage = explode("/", $currentPage)[2];
 		<link href="css/stylesheet.css" rel="stylesheet">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 		
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">
+		
 	</head>
 	<body>
+		<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+		<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+		<script type="text/javascript" src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
 		<div class="container-fluid border">
 			<div class="row align-items-center">
 				<!-- Hamburger Button (visible only on small screens) -->
-				<button class="col-auto d-sm-none btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu">
+				<button class="col-auto d-md-none btn btn-lg" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu">
 					☰
 				</button>
 				<div class="col">
 					<h1>Incident response Portal</h1>
 				</div>
 				<!-- Offcanvas meny-->
-				<div class="offcanvas offcanvas-start d-sm-none border" id="sidebarMenu" data-bs-scroll="true" data-bs-backdrop="true">
-					<div class="offcanvas-header d-block d-sm-none">
+				<div class="offcanvas offcanvas-start d-md-none border" id="sidebarMenu" data-bs-scroll="true" data-bs-backdrop="true">
+					<div class="offcanvas-header">
 						<!-- Close Button -->
 						<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 					</div>
@@ -50,9 +56,9 @@ $currentPage = explode("/", $currentPage)[2];
 				</div>
 				
 				
-				<div class="row gx-4">
+				<div class="row gx-4 mt-3 vh-80 border border-danger">
 					<!-- Sidebar navigation -->
-					<div class="col-auto d-none d-sm-block">
+					<div class="col-auto border border-secondary d-none d-md-block">
 						<ul class="nav nav-pills flex-column">
 						  <li class="nav-item">
 							<a class="nav-link <?php if ($currentPage == 'incident_dashboard.php') { echo 'active'; }?>" href="incident_dashboard.php">Incident Dashboard</a>
@@ -77,6 +83,8 @@ error_reporting(E_ALL);
 require_once("library/tracking.php");
 $footer = <<<END
 			</div>
+		
+		<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 	</body>
 </html>
