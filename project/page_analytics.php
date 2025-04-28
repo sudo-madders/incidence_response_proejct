cc103cdf683896bbc6073d39dc7e3d6b26f153d5
Daave11 <49002232+Daave11@users.noreply.github.com>
2025-04-28 14:38:56 +0200

    Working draft

--- project/page_analytics.php
+++ project/page_analytics.php
@@ -1,4 +1,45 @@
 <?php 
 include("template.php");
+include("library/database.php");
+?>
+<div class="col-md-8 border m-auto">
+	<table id="userTable" class="display table table-striped table-bordered table-hover" style="width:100%">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Page</th>
+				<th>Browser</th>
+				<th>IP</th>
+				<th>Username</th>
+				<th>Timestamp</th>
+			</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>
+</div>
+<script>
+$(document).ready(function() {
+    $('#userTable').DataTable({
+        ajax: {
+            url: 'library/get_data.php',   //  The PHP script that returns the JSON
+            dataSrc: 'data'       //  Tell DataTables where the data is
+        },
+        columns: [
+            { data: 'visit_ID'},
+            { data: 'page'},
+            { data: 'browser'},
+            { data: 'ip'},
+			{ data: 'username'},
+            { data: 'timestamp'}
+        ],
+        scrollCollapse: true,
+        scrollY: '400px',
+		scrollX: '100%'
+    });
+});
+</script>
+<?php
 echo $footer;
+$mysqli->close();
 ?>
