41004ae3701ba93452cacb50f20948598fad8c62
sudo-madders <mahhus24@student.hh.se>
2025-05-13 10:18:10 +0200

    changed the title of the index.php

--- project/index.php
+++ project/index.php
@@ -38,7 +38,7 @@ if (isset($_POST['username']) and isset($_POST['password'])) {
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Bootstrap demo</title>
+		<title>Incident response portal</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	</head>
 	<body>
